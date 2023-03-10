1e775ab21cbf9515fc9f69e3ce1be600eb5c8d0d
wzx <431225046@qq.com>
2023-03-11 01:46:50 +0800

    update

--- src/main/java/cn/takujo/demo/RestConf.java
+++ src/main/java/cn/takujo/demo/RestConf.java
@@ -1,20 +1,31 @@
 package cn.takujo.demo;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestConf {
 
     @Bean
-    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory, ClientHttpRequestInterceptor inter) {
         RestTemplate restTemplate = new RestTemplate(factory);
+        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(inter);
+        restTemplate.setInterceptors(interceptors);
         // 支持中文编码
         restTemplate.getMessageConverters().set(1,
                 new StringHttpMessageConverter(Charset.forName("UTF-8")));
@@ -28,6 +39,19 @@ public class RestConf {
         factory.setReadTimeout(5000);// 单位为ms
         factory.setConnectTimeout(5000);// 单位为ms
         return factory;
+    }
+
+    @Component
+    class Inter implements ClientHttpRequestInterceptor {
+
+        @Override
+        public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
+                throws IOException {
+            var hea = request.getHeaders();
+            hea.remove("Connection");
+            return execution.execute(request, body);
+        }
+
     }
 
 }
